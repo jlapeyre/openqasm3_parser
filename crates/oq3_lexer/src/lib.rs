e61348a030840a35895244e750036863066ac2f3
John Lapeyre <1969884+jlapeyre@users.noreply.github.com>
2025-11-15 21:26:00 -0500

    check

--- crates/oq3_lexer/src/lib.rs
+++ crates/oq3_lexer/src/lib.rs
@@ -262,7 +262,7 @@ fn is_id_continue(c: char) -> bool {
 }
 
 /// The passed string is lexically an identifier.
-pub fn is_ident(string: &str) -> bool {
+fn is_ident(string: &str) -> bool {
     let mut chars = string.chars();
     if let Some(start) = chars.next() {
         is_id_start(start) && chars.all(is_id_continue)
